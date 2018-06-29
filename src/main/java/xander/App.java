6ca008f4fe76bbd144eee44003957fa1198e6060
Xander Beberman <xanderb@cs.uchicago.edu>
2018-06-29 08:20:19 -0500

    updated maven pom

--- src/main/java/xander/App.java
+++ src/main/java/xander/App.java
@@ -1,13 +1,31 @@
 package xander;
 
+import java.io.File;
+import java.io.InputStream;
+import java.io.FileInputStream;
+import java.util.Iterator;
+import org.apache.poi.ss.usermodel.*;
+
 /**
  * Hello world!
  *
  */
 public class App 
 {
-    public static void main( String[] args )
+
+  public static final String doc1 = "./Data1.xlsx";
+  public static final String doc2 = "./Data2.xlsx";
+  
+
+    public static void main(String[] args)
     {
-        System.out.println( "Hello World!" );
+      System.out.println("help");
+      try {
+        Workbook wb1 = WorkbookFactory.create(new File(doc1));
+        Workbook wb2 = WorkbookFactory.create(new File(doc2));
+        System.out.println("workbook1 has " + wb1.getNumberOfSheets() + " Sheets");
+      } catch(Exception ioe) {
+        ioe.printStackTrace();
+      }
     }
 }
