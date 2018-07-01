dbc337db067be28e1eb199951f12b3f4417e7fbb
Xander Beberman <xanderb@cs.uchicago.edu>
2018-07-01 14:32:49 -0500

    mostly working

--- src/main/java/xander/App.java
+++ src/main/java/xander/App.java
@@ -7,7 +7,13 @@ import java.util.Iterator;
 import org.apache.poi.ss.usermodel.*;
 import org.apache.poi.ss.util.*;
 import org.json.*;
-import oracle.json;
+import org.apache.http.HttpEntity;
+import org.apache.http.HttpResponse;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.entity.StringEntity;
+import org.apache.http.entity.StringEntity.UrlEncodedFormEntity;
+import org.apache.http.impl.client.HttpClientBuilder;
 
 /**
  * Hello world!
@@ -20,7 +26,7 @@ public class App
   public static final String doc2 = "./Data2.xlsx";
   public static final int cols = 3;
   public static final int rows = 5;
-  
+  // public static final String postURL = "http://34.239.125.159:5000/challenge";
 
     public static void main(String[] args) {
       System.out.println("help");
@@ -32,9 +38,9 @@ public class App
       try {
         wb1 = WorkbookFactory.create(new File(doc1));
         wb2 = WorkbookFactory.create(new File(doc2));
-
       }  catch(Exception ioe) {
         ioe.printStackTrace();
+        System.exit()
       }
       Workbook books[] = {wb1, wb2};
 
@@ -46,7 +52,7 @@ public class App
         Workbook wb = books[i];
         Dataset ds = sets[i];
         Sheet sheet = wb.getSheetAt(0);
-        // account for header
+        // account for header row
         for (int r = 1; r < rows; r ++) {
           Row row = sheet.getRow(r);
           ds.numSet1[r-1] = (int) row.getCell(0).getNumericCellValue();
@@ -58,32 +64,30 @@ public class App
 
       int numberSetOne[] = new int[rows-1];
       int numberSetTwo[] = new int[rows-1];
-      String words[] = new String[rows-1];
-
-      JSONBuilderFactory factory = Json.createBuilderFactory(config);
-
-      JsonArray numberSetOneJSON = factory.createArrayBuilder();
-      JsonArray numberSetTwoJSON = factory.createArrayBuilder();
-      JsonArray wordSetOneJSON = factory.createArrayBuilder();
+      String wordSetOne[] = new String[rows-1];
 
       for (int i = 0; i < (rows-1); i++) {
         numberSetOne[i] = ds1.numSet1[i] * ds2.numSet1[i];
         numberSetTwo[i] = ds1.numSet2[i] / ds2.numSet2[i];
         wordSetOne[i] = ds1.wordSet[i] + " " + ds2.wordSet[i];
-
-        numberSetOneJSON.add(numberSetOne[i]);
-        numberSetTwoJSON.add(numberSetTwo[i]);
-        wordSetOneJSON.add(wordSetOne[i]);
       }
 
-      JsonObject value = factory.createObjectBuilder()
-        .add("id", "xander@xanderb.com")
-        .add("numberSetOne", numberSetOneJSON)
-        .add("numberSetTwo", numberSetTwoJSON)
-        .add("wordSetOne", wordSetOneJSON)
-        .build();
-      System.out.println(value);
+      String json = new JSONObject()
+        .put("id", "xander@xanderb.com")
+        .put("numberSetOne", numberSetOne)
+        .put("numberSetTwo", numberSetTwo)
+        .put("wordSetOne", wordSetOne)
+        .toString();
+      System.out.println(json);
 
-      
+      String postURL = "http://ptsv2.com/t/gztfh-1530466570/post";
+      HttpClient httpClient = HttpClientBuilder.create().build();
+      HttpPost post = new HttpPost(postURL);
+      post.setHeader("Content-type", "application/json");
+      post.setHeader("Accept", "application/json");
+      post.setEntity(new UrlEncodedFormEntity(json));
+      HttpResponse response = httpClient.execute(post);
+      System.out.println(response.toString());
+
     }
 }
